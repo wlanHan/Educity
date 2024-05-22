4abd17ca81edb3937fff16eee7ee9c245a6e79fb
wlanHan <hanstudiocontact@gmail.com>
2024-05-22 22:47:03 +0300

    hero section +

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Hero from './Components/Hero/Hero'
+import Programs from './Components/Programs/Programs'
+import Title from './Components/Title/Title'
 
 const App = () => {
   return (
     <>
       <Navbar />
+      <Hero />
+      <div className="container">
+        <Title subTitle='Our Program' title='What We Offer' />
+        <Programs />
+      </div>
     </>
   )
 }
