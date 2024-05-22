5533939fbd3d49276e0ef73718149cdfff9fbd76
wlanHan <hanstudiocontact@gmail.com>
2024-05-22 23:27:36 +0300

    dark navbar +

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ const App = () => {
         <Title subTitle='Our Program' title='What We Offer' />
         <Programs />
         <About />
+        <Title subTitle='Gallery' title='Campus Photos' />
       </div>
     </>
   )
